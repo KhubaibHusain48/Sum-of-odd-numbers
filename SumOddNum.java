48d4bc6dd094c68ca1fc6668af29b287df0814f1
Khubaib Husain <khubaibhusain45@gmail.com>
2025-06-21 23:15:58 +0500

    Update SumOddNum.java

--- SumOddNum.java
+++ SumOddNum.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
 
-public class SumofOddNum {
+public class OddSumWithDisplay {
     public static void main(String[] args) {
-        sumdigit();
-    }
-
-    public static void sumdigit() {
-        int sum = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0) {
-                continue;
-            } else {
-                sum += i;
-            }
-        }
-        System.out.println("Sum of odd numbers from 1 to " + n + " is " + sum);
-    }
 
+        // Input from user
+        System.out.print("Enter a number n: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        System.out.print("Odd numbers from 1 to " + n + ": ");
+        for (int i = 1; i <= n; i += 2) {
+            System.out.print(i + " ");
+            sum += i;
+        }
+
+        System.out.println("\nSum of odd numbers: " + sum);
+    }
 }
